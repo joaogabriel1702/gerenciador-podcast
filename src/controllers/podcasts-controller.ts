b5c17e1f096f4451a6387f63b8219cca6619b925
joaogabriel1702 <joaogabrielcsoares@hotmail.com>
2025-09-04 22:29:30 -0300

    iniciando o services

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,8 +1,10 @@
 import {IncomingMessage, ServerResponse} from 'http'
+import {serviceListEpisodes} from '../services/list-episodes-service'
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+  
+  const content = await serviceListEpisodes()
+  
     res.writeHead(200, {"Content-Type": "application/json"})
-    res.end({
-        name:"Joao",
-    })
+    res.end(JSON.stringify(content))
 }
